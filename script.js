5cabb587526690f38e1108ea9ae96831f1a6abe6
afraahhh04 <afraahfatima04@gmail.com>
2024-05-20 20:51:32 +0530

    adding loop n i/p is no.

--- script.js
+++ script.js
@@ -2,11 +2,28 @@ const prompt = require('prompt-sync')();
 //new method
 const deposit=()=>{
     console.log('welcome');
-    const amount = prompt('enter deposit amount');
-    return amount;
-    }
+    let totalAmount = 0;
+    let keepGoing = true;
 
-    console.log (deposit());
+    while (keepGoing) {
+    const amount = prompt('enter deposit amount :');
+
+// checking if input is number
+if (isNaN(amount) || amount.trim() === ''){
+    console.log('error:please enter a valid number');
+    continue;
+}
+totalAmount += Number(amount);
+console.log(`Current total deposit amount: $${totalAmount}`); //shows the current total amount
+
+const continueDeposit = prompt('do you want to deposit more? (yes/no):')
+if ( continueDeposit !=='yes') {
+    keepGoing = false;
+}
+    }
+    return totalAmount;
+}
+    console.log(`Final deposit amount: $${deposit()}`);
 
 
 
